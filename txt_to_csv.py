15a2cd4de4b50cd0f5bcfeecc9dab99babe5202f
Nicholas Rucker <nick@29.216.21.198.tigernet.wifi.dyn.clemson.edu>
2019-09-02 16:47:42 -0400

    added glob functionality -will probably clean up a bit

--- txt_to_csv.py
+++ txt_to_csv.py
@@ -1,4 +1,5 @@
 import csv
+import glob
 
 # Asking the user if they would like to convert multiple or just one file
 # We are expecting an answer of '1' or '2' so we will need to validate the input
@@ -10,16 +11,39 @@ while userChoice != '1' and userChoice != '2':
 	userChoice = input()
 
 if userChoice == '1':
-	print("Enter the path to the target directory: ")
+	print("Are the files in your current working directory? (Y or N)")
 	target = input()
+	if target.upper() == 'Y':
+		target = ''
+	else:
+		print("Enter the path to the target directory: ")
+		tempTarget = input()
 
 else:
 	print("Enter the full file name (including .txt file extension): ")
 	target = input()
 
-if userChoice == '2':
+
+if userChoice == '1':
+	if tempTarget != '':
+		if tempTarget[len(tempTarget) - 1] == '\\':
+			target = tempTarget[:len(tempTarget) - 1] + '/'
+			print("new target is: " + target)
+		elif tempTarget[len(tempTarget) - 1] != '/':
+			target = tempTarget + '/'
+	print(target)
+	for file in glob.glob(target + "*.txt"):
+		print(file)
+		with open(file, 'r') as in_file:
+		    stripped = (line.strip() for line in in_file)
+		    lines = (line.split(",") for line in stripped if line)
+		    with open(file.replace('.txt', '.csv'), 'w') as out_file:
+		        writer = csv.writer(out_file)
+		        writer.writerows(lines)
+
+else:
 	with open(target, 'r') as in_file:
-	    stripped = (line.replace('\t', ',').replace('setosa', '1').replace('versicolor', '2').replace('virginica', '3').strip() for line in in_file)
+	    stripped = (line.strip() for line in in_file)
 	    lines = (line.split(",") for line in stripped if line)
 	    with open(target.replace('.txt', '.csv'), 'w') as out_file:
 	        writer = csv.writer(out_file)
