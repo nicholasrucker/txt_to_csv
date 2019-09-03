8abb4e9d6d4703bb98d1152d6ae3e623cbe946e3
nicholasrucker <43759871+nicholasrucker@users.noreply.github.com>
2019-09-03 00:38:07 -0400

    added some documentation
    i can probably simplify this a little bit
    **also, i have to add functionality for converting a single file in a different directory**

--- txt_to_csv.py
+++ txt_to_csv.py
@@ -2,14 +2,15 @@ import csv
 import glob
 
 # Asking the user if they would like to convert multiple or just one file
-# We are expecting an answer of '1' or '2' so we will need to validate the input
 print("Would you like to enter a target directory(1) or file name(2)?")
 userChoice = input()
 
+# We are expecting an answer of '1' or '2' so we will need to validate the input
 while userChoice != '1' and userChoice != '2':
-	print("Enter '1' if you would like to convert a whole directory of files or '2' if you would like to convert a single file")
+	print("Enter '1' if you would like to convert multiple files or '2' if you would like to convert a single file")
 	userChoice = input()
 
+# Now we just need to figure out where the files are
 if userChoice == '1':
 	print("Are the files in your current working directory? (Y or N)")
 	target = input()
@@ -18,12 +19,13 @@ if userChoice == '1':
 	else:
 		print("Enter the path to the target directory: ")
 		tempTarget = input()
-
-else:
+		
+# Only the file name is needed if we are converting a single file
+else:	
 	print("Enter the full file name (including .txt file extension): ")
 	target = input()
 
-
+# Converting a glob of files
 if userChoice == '1':
 	if tempTarget != '':
 		if tempTarget[len(tempTarget) - 1] == '\\':
@@ -38,7 +40,7 @@ if userChoice == '1':
 		    with open(file.replace('.txt', '.csv'), 'w') as out_file:
 		        writer = csv.writer(out_file)
 		        writer.writerows(lines)
-
+# Just converting one
 else:
 	print("Transforming", target)
 	with open(target, 'r') as in_file:
